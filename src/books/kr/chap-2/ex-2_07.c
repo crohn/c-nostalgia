eb05d616ab0a0dcab534ad03ce2c98c7139d7b8d
Davide Giuseppe Monaco <monaco.dg@gmail.com>
2025-03-22 23:59:10 +0100

    fix: fix ex-2_07 implementation

--- src/books/kr/chap-2/ex-2_07.c
+++ src/books/kr/chap-2/ex-2_07.c
@@ -5,18 +5,22 @@
  * leaving the others unchanged. */
 
 #include <stdio.h>
+#include "kr_bitwise.h"
 
 /* The function invert should make a copy of x and apply a XOR on a masked
  * section. The print_binary utility function helps debugging. */
 
 unsigned invert(unsigned x, int p, int n);
 
-void print_binary(unsigned x);
-
 int main()
 {
-  print_binary(0xffu);
+  unsigned value = 0xa1u;
+
+  kr_print_binary(value);
   printf("\n");
+  kr_print_binary(invert(value, 7, 5));
+  printf("\n");
+
   return 0;
 }
 
@@ -34,44 +38,19 @@ unsigned invert(unsigned x, int p, int n)
 {
   unsigned mask;
 
-  /* validate p and n */
+  if (n == 0)
+    return x;
+
+  if (n < 0 || n > p + 1) {
+    printf("ERROR: n must me such that 0 <= n <= p + 1, got n:%d, p:%d\n", n, p);
+    return x;
+  }
 
   /* create mask */
-  mask = ~(~0u << n) << (p + 1); /* mask -> 0011 1100 ... 0000 */
+  mask = ~(~0u << n) << (p + 1 - n); /* mask -> 0011 1100 ... 0000 */
 
-  return (x & ~mask) | ((x ^ x) & mask);
+  return (x & ~mask) | (~x & mask);
 }
 
-/**
- * print_binary() - Print the provided integral number in binary format.
- * @x: The unsigned integral number to print.
- *
- * The binary string is produced by bitwise-and'ing the provided value with a
- * moving mask of 1 bit, shifting from the leftmost to the rightmost position.
- * 
- * The mask is obtained by leveraging the fact that ~0u does not carry the sign
- * bit when right-shifting, so:
- *
- *     ~0u       -> 1111 1111 ... 1111
- *     ~0u >> 1  -> 0111 1111 ... 1111
- *   ~(~0u >> 1) -> 1000 0000 ... 0000
- *
- * Formatting -- The binary string is printed starting from the MSB and for
- * easier readability, a whitespace is introduced every four characters.
- * Furthermore, no newline is added at the end of the binary string.
- */
-void print_binary(unsigned x)
-{
-  unsigned mask = ~(~0u >> 1); /* mask -> 1000 0000 ... 0000  */
-  int i = 0;                   /* used to print spacing every 4 bits */
 
-  while (mask > 0) {
-    printf("%c", x & mask ? '1' : '0');
-    mask >>= 1;
-
-    /* `mask > 1` avoids trailing space */
-    if (i++ % 4 == 3 && mask > 1)
-      printf(" ");
-  }
-}
 
